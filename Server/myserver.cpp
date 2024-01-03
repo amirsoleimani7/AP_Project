fb8e26d66d7b72c5baba882c772f390c61304ffa
MMA <mohamad.a.131990@gmail.com>
2024-01-04 00:07:51 +0330

    server created and socket functions set

--- Server/myserver.cpp
+++ Server/myserver.cpp
@@ -7,6 +7,14 @@ myServer::myServer(QWidget *parent)
 
 {
     ui->setupUi(this);
+    tcpServe = new QTcpServer;
+    if (tcpServe->listen(QHostAddress::AnyIPv4,43000)){
+        connect(tcpServe,&QTcpServer::newConnection,this,&myServer::newConnection);
+        ui->statusbar->showMessage("Tcp started");
+    }
+    else{
+        QMessageBox::information(this, "tcp error ",tcpServe->errorString());
+    }
 }
 
 myServer::~myServer()
@@ -15,4 +23,49 @@ myServer::~myServer()
     delete ui;
 }
 
+void myServer::readSocket()
+{
+
+}
+
+void myServer::discardsocket()
+{
+    QTcpSocket* socket = reinterpret_cast<QTcpSocket*>(sender());
+    int id = clientList.indexOf(socket);
+    if (id>-1){
+        clientList.removeAt(id);
+    }
+    ui->clientListCombo->clear();
+    foreach (QTcpSocket* sockettemp, clientList) {
+        ui->clientListCombo->addItem(QString::number(sockettemp->socketDescriptor()));
+    }
+    socket->deleteLater();
+}
+
+void myServer::newConnection()
+{
+    while(tcpServe->hasPendingConnections()){
+        addToSocketList(tcpServe->nextPendingConnection());
+    }
+
+}
+
+void myServer::addToSocketList(QTcpSocket *socket)
+{
+    clientList.append(socket);
+    connect(socket,&QTcpSocket::readyRead,this, &myServer::readSocket);
+    connect(socket,&QTcpSocket::disconnected,this,myServer::discardsocket);
+    ui->textEditMasages->append("Client is connected : socket : " + Qstring::number(socket->socketDescriptor()));
+    ui->clientListCombo->addItem(Qstring::number(socket->socketDescriptor()));
+}
+
+void myServer::on_sendFileBTN_clicked()
+{
+
+}
+
+void myServer::sendFile(QTcpSocket *socket, QString fileName)
+{
+
+}
 
--- Server/myserver.h
+++ Server/myserver.h
@@ -2,7 +2,11 @@
 #define MYSERVER_H
 
 #include <QMainWindow>
-#include "person.h"
+#include <QtNetwork>
+#include <QtGui>
+#include <QtCore>
+#include <QtWidgets>
+
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -17,8 +21,21 @@ class myServer : public QMainWindow
 public:
     myServer(QWidget *parent = nullptr);
     ~myServer();
+private slots:
+    void readSocket();
+    void discardsocket();
+    void newConnection();
+    void addToSocketList(QTcpSocket* socket);
+    void on_sendFileBTN_clicked();
+
+private:
+    void sendFile (QTcpSocket* socket,QString fileName);
 
 private:
     Ui::myServer *ui;
+
+    QTcpServer *tcpServe;
+    QList<QTcpSocket> clientList;
+
 };
 #endif // MYSERVER_H
