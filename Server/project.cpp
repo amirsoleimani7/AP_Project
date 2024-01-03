c7a664a6d6979b92acc5a5d9ec50d55ee3f0e8b3
MMA <mohamad.a.131990@gmail.com>
2024-01-03 18:27:11 +0330

    adding id and copy cu.. and == to project class

--- Server/project.cpp
+++ Server/project.cpp
@@ -26,9 +26,55 @@ QVector<QString> Project::getProjectTasks() const
     return projectTasks;
 }
 
-Project::Project(QString PN, int PT):projectName{PN},projectType{PT}
+int Project::getLastProjectId() const
 {
+    return lastProjectId;
+}
 
+void Project::setLastProjectId(int newLastProjectId)
+{
+    lastProjectId = newLastProjectId;
+}
+
+int Project::getProjectId() const
+{
+    return projectId;
+}
+
+void Project::setProjectName(const QString &newProjectName)
+{
+    projectName = newProjectName;
+}
+
+void Project::setProjectType(int newProjectType)
+{
+    projectType = newProjectType;
+}
+
+void Project::setProjectPerson(const Person &newProjectPerson)
+{
+    projectPerson = newProjectPerson;
+}
+
+bool Project::operator ==(const Project &other)
+{
+    return (projectId == other.projectId);
+}
+
+Project::Project(QString PN, int PT):projectName{PN},projectType{PT},projectId{projectIdGenerator + lastProjectId}
+{
+    projectIdGenerator ++;
+}
+
+Project::Project(const Project &other)
+{
+    projectId = other.projectId;
+    projectName = other.projectName;
+    projectPerson = other.projectPerson;
+    projectTasks = other.projectTasks;
+    projectTeams = other.projectTeams;
+    projectType = other.projectType;
+    lastProjectId = other.lastProjectId;
 }
 
 Project::~Project()
