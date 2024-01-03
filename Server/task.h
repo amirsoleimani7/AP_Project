7c1ee3882d38c31292ad3beb8c4a0853f09f5df0
MMA <mohamad.a.131990@gmail.com>
2024-01-03 16:00:35 +0330

    task class updated

--- Server/task.h
+++ Server/task.h
@@ -1,6 +1,8 @@
 #ifndef TASK_H
 #define TASK_H
 #include <QString>
+#include "project.h"
+#include "person.h"
 
 class Task
 {
@@ -10,11 +12,25 @@ private:
     int lastTaskId;
     QString taskText;
     Project taskProject;
+    Person taskPerson;
+    bool isDone;
     //date
     int priority;
 
 public:
-    Task();
+    Task(Project pro,QString TT,Person pers,int prio);
+    int getTaskId() const;
+    int getLastTaskId() const;
+    QString getTaskText() const;
+    Project getTaskProject() const;
+    int getPriority() const;
+    void setTaskText(const QString &newTaskText);
+    void setPriority(int newPriority);
+    void setTaskPerson(const Person &newTaskPerson);
+    void Done();
+    void UnDone();
+    Person getTaskPerson() const;
+    bool getIsDone() const;
 };
 
 #endif // TASK_H
